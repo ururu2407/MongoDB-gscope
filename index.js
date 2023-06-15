b461360e00ae9cafdc55be313dd828d60198f69c
ururu2407 <122101396+ururu2407@users.noreply.github.com>
2023-06-15 18:56:11 +0300

    fix

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ import { UserController, PostController } from './controllers/index.js';
 
 mongoose
     .connect
-    (
-        'mongodb+srv://aklokun2407:kozadenuwtak@cluster0.qprfrz8.mongodb.net/blog?retryWrites=true&w=majority'
-    )
+    (process.env.MONGODB_URI)
     .then(() => console.log('DB ok'))
     .catch(() => console.log('DB error', err));
 
@@ -54,7 +52,7 @@ app.delete('/posts/:id', checkAuth, PostController.remove);
 app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
     if (err) {
         return console.log(err);
     }
